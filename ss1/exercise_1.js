7313bfc682fe8069e2fea4cd1ee30a52415c83a7
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-08-17 16:36:18 +0700

    nop bai tap

---
+++ ss1/exercise_1.js
@@ -0,0 +1,62 @@
+let courses = [
+  {
+    id: 1,
+    title: "ReactJS Tutorial",
+    rating: 4.2,
+  },
+  {
+    id: 2,
+    title: "Angular Tutorial",
+    rating: 2.5,
+  },
+  {
+    id: 3,
+    title: "VueJS Tutorial",
+    rating: 3.8,
+  },
+  {
+    id: 4,
+    title: "Java Tutorial",
+    rating: 4,
+  },
+  {
+    id: 5,
+    title: "JavaScript Tutorial",
+    rating: 3.5,
+  },
+];
+
+let addedCourses = [
+  {
+    id: 6,
+    title: "PHP Tutorial",
+    rating: 3,
+  },
+  {
+    id: 7,
+    title: "C# Tutorial",
+    rating: 2,
+  },
+  {
+    id: 8,
+    title: "Docker Tutorial",
+    rating: 3.8,
+  }
+];
+
+// First Request
+const firstRequest = courses.filter(course => course.rating >= 4);
+console.log(firstRequest);
+
+// Second Request
+const secondRequest = courses
+  .filter(course => course.rating < 4)
+  .map(course => {
+    let { id, title, rating } = course;
+    return id + " - " + title + " - " + rating;
+  });
+console.log(secondRequest);
+
+// Third Request
+const mergeCourses = [...courses, ...addedCourses];
+console.log(mergeCourses);
