32e83460c9110942798c5e19148c1b1fb317de23
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-08-24 21:22:01 +0700

    update

--- ss6/exercise/ex_2/src/components/EditBook.js
+++ ss6/exercise/ex_2/src/components/EditBook.js
@@ -8,11 +8,7 @@ import * as Yup from 'yup';
 const EditBook = () => {
     const navigate = useNavigate();
     const { id } = useParams();
-    const [book, setBook] = useState({
-        id: "231",
-        title: "",
-        quantity: "0"
-    });
+    const [book, setBook] = useState();
 
     const getById = async () => {
         const currentBook = await libraryService.getBookById(id);
@@ -24,42 +20,47 @@ const EditBook = () => {
         return status;
     }
     useEffect(() => {
-        getById();
-    }, [])
+        if(book === undefined) {
+            getById();
+        }
+    
+    }, [book])
 
     return (
 
         <>
             <h1>Update book</h1>
-            
-            <Formik
+            { book !== undefined  ? 
+             <Formik
 
-                initialValues={{
-                    myField: book   
-                }}
+             initialValues={{
+                 title: book.title,
+                 quantity: book.quantity
+             }}
 
-                validationSchema={Yup.object({
-                    title: Yup.string()
-                        .required("title is not empty"),
-                    quantity: Yup.number()
-                        .required("quantity is not empty")
-                })}
-                onSubmit={async (values, { setSubmitting }) => {
-                    setSubmitting(false);
-                    updateBook(values);
-                    navigate('/');
-                }}
-            >
-                <Form>
-                    <div for='title'>Title</div>
-                    <Field id='title' name='title' />
-                    <ErrorMessage component='small' name='title' /><br />
-                    <div>Quantity</div>
-                    <Field id='quantity' name='quantity' />
-                    <ErrorMessage component='small' name='quantity' /><br />
-                    <button>Submit</button>
-                </Form>
-            </Formik>
+             validationSchema={Yup.object({
+                 title: Yup.string()
+                     .required("title is not empty"),
+                 quantity: Yup.number()
+                     .required("quantity is not empty")
+             })}
+             onSubmit={async (values, { setSubmitting }) => {
+                 setSubmitting(false);
+                 updateBook(values);
+                 navigate('/');
+             }}
+         >
+             <Form>
+                 <div for='title'>Title</div>
+                 <Field id='title' name='title' />
+                 <ErrorMessage component='small' name='title' /><br />
+                 <div>Quantity</div>
+                 <Field id='quantity' name='quantity' />
+                 <ErrorMessage component='small' name='quantity' /><br />
+                 <button>Submit</button>
+             </Form>
+         </Formik> : ""}
+           
         </>
     )
 }
