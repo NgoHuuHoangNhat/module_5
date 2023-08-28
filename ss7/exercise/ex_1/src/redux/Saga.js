ae6883c81b528470fecb0a692ecd77eb2560e388
hoangnhat3103@gmail.com <hoangnhat3103@gmail.com>
2023-08-28 19:08:22 +0700

    update

--- ss7/exercise/ex_1/src/redux/Saga.js
+++ ss7/exercise/ex_1/src/redux/Saga.js
@@ -1,7 +1,6 @@
 import { put, takeLatest } from 'redux-saga/effects';
-import { GET_ALL_USER_LIST, DELETE_USER, GET_ALL_USER_LIST_SUCCESS } from '../redux/Types';
+import { GET_ALL_USER_LIST, DELETE_USER, GET_ALL_USER_LIST_SUCCESS, DELETE_USER_SUCCESS } from '../redux/Types';
 import { getAllUser , deleteUser } from '../services/UserService';
-import { useLayoutEffect } from 'react';
 
 
 function* getUserList() {
@@ -12,13 +11,19 @@ function* getUserList() {
     });
 }
 
-function* deleteUser(){
-    const result = yield deleteUser()
+function* deleteUserInList(payload){
+    const result = yield deleteUser(payload.payload);
+    if(result.status === 200){
+        alert("respone status" + result.status)
+    }
+    yield put({
+        type: GET_ALL_USER_LIST,
+    })
 }
 
 function* usersSaga() { 
     yield takeLatest(GET_ALL_USER_LIST, getUserList);
-    yield takeLatest(DELETE_USER, deleteUser);
+    yield takeLatest(DELETE_USER, deleteUserInList);
     
 }
 
--- ss7/exercise/ex_1/src/redux/reducers/UserReducer.js
+++ ss7/exercise/ex_1/src/redux/reducers/UserReducer.js
@@ -1,12 +1,10 @@
-import { GET_ALL_USER_LIST, DELETE_USER, GET_ALL_USER_LIST_SUCCESS } from '../Types';
+import { GET_ALL_USER_LIST_SUCCESS } from '../Types';
 
 const initState = [];
 
 const userReducer = (state = initState, action) => {
     const { type, payload } = action;
     switch (type) {
-        case DELETE_USER:
-            return state
         case GET_ALL_USER_LIST_SUCCESS:
             return payload;
         default:
